943fbb6827b563243b66eeaa6830e1f7d41865a0
zaid <zald0001@student.monash.edu>
2022-12-21 13:55:17 +0800

    scroll indicater is hidden when scrolling down

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 // Tooltip
 
 $('#copy').tooltip({
-    trigger: 'click',
-    placement: 'bottom'
-  });
-  
-  function setTooltip(message) {
-    $('#copy').tooltip('hide')
-      .attr('data-original-title', message)
-      .tooltip('show');
+  trigger: 'click',
+  placement: 'bottom'
+});
+
+function setTooltip(message) {
+  $('#copy').tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show');
+}
+
+function hideTooltip() {
+  setTimeout(function () {
+    $('#copy').tooltip('hide');
+  }, 1000);
+}
+
+// Clipboard
+var clipboard = new ClipboardJS('#copy');
+
+clipboard.on('success', function (e) {
+  setTooltip('Copied!');
+  hideTooltip();
+
+});
+
+clipboard.on('error', function (e) {
+  setTooltip('Failed!');
+  hideTooltip();
+
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop()  < 400) {
+    $('.scroll-indicator').hide();
+    $('.scroll-indicator').show();
+  } else {
+    $('.scroll-indicator').show();
+    $('.scroll-indicator').hide();
   }
-  
-  function hideTooltip() {
-    setTimeout(function() {
-      $('#copy').tooltip('hide');
-    }, 1000);
-  }
-  
-  // Clipboard
-  var clipboard = new ClipboardJS('#copy');
-  
-  clipboard.on('success', function(e) {
-       setTooltip('Copied!');
-       hideTooltip();
-   
-  });
-  
-  clipboard.on('error', function(e) {
-      setTooltip('Failed!');
-      hideTooltip();
-    
-  });
+});
